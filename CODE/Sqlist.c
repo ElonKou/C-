6cc15a4edeec567714c6018191234566b5517e68
ElonKou <kouzi@ktime.cc>
2016-10-25 17:26:21 +0800

    上课前提交的

--- CODE/Sqlist.c
+++ CODE/Sqlist.c
@@ -6,6 +6,7 @@
 #define LIST_INIT_SIZE  100
 #define LISTINCREAMENT  10
 
+//初始化列表
 Status InitList_Sq(Sqlist *L){
     L->elem = (ElemType*)malloc(LIST_INIT_SIZE*sizeof(ElemType));
     if(L->elem){
@@ -16,22 +17,35 @@ Status InitList_Sq(Sqlist *L){
     return OK;
 }
 
+//插入一个节点
 Status ListInsert_Sq(Sqlist *L,int i,ElemType e){
     //判断插入位置是否合法
     if(i<1||i>L->length+1){
         return ERROR;
     }
     if(L->length >= L->listSize){
-        L->elem = (ElemType*)malloc(LIST_INIT_SIZE * sizeof(ElemType));
+        //L->elem = (ElemType*)malloc(LIST_INIT_SIZE * sizeof(ElemType));
         ElemType* newbase = (ElemType*)realloc(L->elem,sizeof(ElemType)*(L->listSize+LISTINCREAMENT));
+        if(!newbase){
+            exit(ERROR);
+        }
+        int q = &(L->elem[i-1]);
     }
+
     return OK;
 }
 
+//得到一个节点的数据
+
+
 int main(){
     Sqlist L;
+    ElemType e;
     InitList_Sq(&L);
     ListInsert_Sq(&L,0,23);
+    ListInsert_Sq(&L,1,32);
+    ListInsert_Sq(&L,2,555);
+    printf("L1:%d",L.elem);
     return 0;
 }
 
